69391fe7acb14485cacd300a9759a9579700101b
blessly-pera <70497919+blessly-pera@users.noreply.github.com>
2021-01-05 03:20:54 +0800

    icons added

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faMugHot } from '@fortawesome/pro-solid-svg-icons';
+
+library.add(faMugHot);
+
+
 export default function Home() {
   return (
     <div>
@@ -5,14 +12,16 @@ export default function Home() {
         <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
           <div class="relative flex items-center justify-between h-16">
             <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-              <div class="flex-shrink-0 flex items-center">
-                <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
-                <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />
+              <div class="flex-shrink-0 flex items-center font-semibold text-white">
+                <FontAwesomeIcon icon={['fas', 'mug-hot']} size="2x"/>
+                <div class="pl-5">
+                  Company Name
+                </div>
               </div>
             </div>
             <div class="ml-3 relative">
             <div>
-              <button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-500 hover:bg-indigo-700">
+              <button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-indigo-500 hover:bg-yellow-600">
 		            Get Started
 		          </button>
             </div>
